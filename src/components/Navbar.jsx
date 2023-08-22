5c4f8832da3ea946f31b2ea6f347a50d0b6980b6
Alejou343 <alejandro.auribe1@gmail.com>
2023-08-22 18:59:10 -0500

    Se añadió Home responsive y variables a tailwind.config.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Navbar = () => {
     return (
         <nav className="flex justify-between px-16 mt-4 items-center">
             <ul className="flex w-2/4">
                 <li> ✓ </li>
-                <li className="font-bold"> VoteChoice </li>
+                <li className="font-semibold font-texts"> VoteChoice </li>
             </ul>
             <ul className="flex justify-between px-16 w-2/4 items-center">
-                <li className="text-purple-900">Home</li>
-                <li className="text-purple-900">Sobre nosotros</li>
-                <li className="text-purple-900">Contacto</li>
-                <li className="bg-purple-900 text-white font-semibold rounded-3xl px-4 py-2">Empezar</li>
+                <li className="text-primaryPurple font-texts hidden md:block">Home</li>
+                <li className="text-primaryPurple font-texts hidden md:block"> About </li>
+                <li className="text-primaryPurple font-texts hidden md:block">Contacto</li>
+                <li className="bg-primaryPurple text-secondaryWhite font-semibold rounded-3xl px-4 py-2 hidden md:block">Empezar</li>
+                
+                <li className="absolute top-4 right-12 block md:hidden">
+                    <Image src="/menuIcon.svg" alt="MenuIcon" width={30} height={30} />
+                </li>
             </ul>
         </nav>
     )
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,11 @@
 import Navbar from "@/components/Navbar";
+import Init from "@/components/Init";
 
 export default function Home() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Init />
+    </>
   )
 }
