6878d99abe0af42f8746d062d251135bf57cfae4
Alejou343 <alejandro.auribe1@gmail.com>
2023-08-23 17:45:41 -0500

    Modificaciones a index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Init from "@/components/Init";
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Init />
+      
     </>
   )
 }
