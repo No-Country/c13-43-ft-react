81e66de5ebd769892c1e9067e365ca4ecdfd55bb
mbernallaura <mbernallaura@gmail.com>
2023-08-25 18:28:35 -0500

    mejorando el sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,40 +3,44 @@ import Image from 'next/image'
 
 const Sidebar = () => {
     return (
-        <div className='bg-primaryPurple flex flex-col'>
+        <div className='bg-primaryPurple h-screen absolute top-0 w-72 pl-20 rounded-tr-md'>
             <ul>
-                <li className=''> 
-                    <a href="" className='flex gap-2'>
-                        <Image src="/Images/panel/panelN.png" width={ 14 } height={ 20 }/>
-                        <span> Panel </span>
+                <li className='p-3'>
+                    <Image src="/Images/logoPanel.png" width={ 186 } height={ 45 }/>
+                </li>
+
+                <li className='pt-9 py-4'> 
+                    <a href="" className='flex gap-3'>
+                        <Image src="/Images/panel/panelN.png" width={ 20 } height={ 20 }/>
+                        <span className='dmsans font-medium'> Panel </span>
                     </a>
                 </li>
 
-                <li className=''> 
-                    <a href="" className='flex gap-2'>
-                        <Image src="/Images/panel/salaN.png" width={ 14 } height={ 20 }/>
-                        <span> Sala </span>
+                <li className='py-4'> 
+                    <a href="" className='flex gap-3'>
+                        <Image src="/Images/panel/salaN.png" width={ 20 } height={ 20 }/>
+                        <span className='dmsans font-medium'> Sala </span>
                     </a>
                 </li>
 
-                <li className=''> 
-                    <a href="" className='flex gap-2'>
-                        <Image src="/Images/panel/votacionN.png" width={ 14 } height={ 20 }/>
-                        <span> Votaciones </span>
+                <li className='py-4'> 
+                    <a href="" className='flex gap-3'>
+                        <Image src="/Images/panel/votacionN.png" width={ 20 } height={ 20 }/>
+                        <span className='dmsans font-medium'> Votaciones </span>
                     </a>
                 </li>
 
-                <li className=''> 
-                    <a href="" className='flex gap-2'>
-                        <Image src="/Images/panel/perfilN.png" width={ 14 } height={ 20 }/>
-                        <span> Perfil </span>
+                <li className='py-4'> 
+                    <a href="" className='flex gap-3'>
+                        <Image src="/Images/panel/perfilN.png" width={ 20 } height={ 20 }/>
+                        <span className='dmsans font-medium'> Perfil </span>
                     </a>
                 </li>
 
-                <li className=''> 
-                    <a href="" className='flex gap-2'>
-                        <Image src="/Images/panel/logoutN.png" width={ 14 } height={ 20 }/>
-                        <span> Salir </span>
+                <li className='py-4'> 
+                    <a href="" className='flex gap-3'>
+                        <Image src="/Images/panel/logoutN.png" width={ 20 } height={ 20 }/>
+                        <span className='dmsans font-medium'> Salir </span>
                     </a>
                 </li>
             </ul>
