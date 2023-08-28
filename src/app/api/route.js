64e4e17accc46efbdbdf3f317e957b8096a70fa7
PabloMur <polillomurillo@gmail.com>
2023-08-28 15:03:56 -0300

    feature: se conecto la base de datos

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,14 +1,59 @@
+import { firestoreDB } from "@/lib/firebaseConn";
 import { NextResponse } from "next/server";
 
-// Simulación de una base de datos de opciones con votos
-//const optionsData = {
-//  option1: { votes: 0 },
-//  option2: { votes: 0 },
-// ... otras opciones ...
-//};
+//TEST: trae todos los usuarios
+export async function GET(request) {
+  try {
+    const allUsersRef = await firestoreDB.collection("users").get();
+    const userData = allUsersRef.docs.map((doc) => doc.data());
 
-export async function POST(request) {
-  const { searchParams } = new URL(request.url);
-  const optionId = searchParams.get("optionId");
-  console.log(optionId);
+    return NextResponse.json({ ok: userData });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Error fetching users" }, 500);
+  }
+}
+
+export async function PUT(request) {
+  try {
+    const { email, data } = await request.json();
+    //Referncia a la collection
+    const querySnapshot = await firestoreDB
+      .collection("users")
+      .where("email", "==", email)
+      .get();
+    //Pregunta si el array NO esta vacio, procedemos porque quiere decir que existe
+    if (!querySnapshot.empty) {
+      //Hacemos una referencia a el elemento de la primera posicion
+      const userRef = querySnapshot.docs[0].ref;
+      //Nos traemos la data del documento
+      const currentUserData = (await userRef.get()).data();
+      //
+      const updatedData = { ...currentUserData, ...data };
+      await userRef.update(updatedData);
+      return NextResponse.json({ ok: updatedData });
+    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Error fetching users" }, 500);
+  }
+}
+
+export async function DELETE(request) {
+  try {
+    const { email } = await request.json();
+    const querySnapshot = await firestoreDB
+      .collection("users")
+      .where("email", "==", email)
+      .get();
+
+    if (!querySnapshot.empty) {
+      const userRef = querySnapshot.docs[0].ref;
+      await userRef.delete();
+      return NextResponse.json({ userDeleted: true });
+    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Error fetching users" }, 500);
+  }
 }
