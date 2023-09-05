9cf23abf7186e921e6d69019a60ab8f0065d7763
PabloMur <polillomurillo@gmail.com>
2023-09-05 16:02:43 -0300

    Se crean Api calls para los hooks

--- src/app/api/notification/route.js
+++ src/app/api/notification/route.js
@@ -1,6 +1,5 @@
 //Se va a notificar a los participantes que la encuesta termino
 import { NextResponse } from "next/server";
-//Endpoint para desloguear un usuario
 export async function POST(request) {
   const body = await request.json();
   const { emails } = body;
