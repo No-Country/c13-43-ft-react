fd875b8c4437cf6c07c85602948dec62671ed08b
Alejou343 <alejandro.auribe1@gmail.com>
2023-09-05 19:34:45 -0500

    Mejoras a la UI Flujo completo de eliminar - crear - compartir Filtrado de salas

--- src/components/Votaciones.jsx
+++ src/components/Votaciones.jsx
@@ -1,5 +1,9 @@
+"use client"
 import React from 'react';
 import Image from 'next/image';
+import ModalGeneral from '@/containers/ModalGeneral';
+import ModalEliminarSala from './ModalEliminarSala';
+import ModalCopiar from './ModalCopiar';
 
 const historial = [
     {codigo: '01563', nombre: 'Fiesta de cumpleaños', ganador: '80% votó 20 de Marzo', isActive: true},
@@ -9,17 +13,28 @@ const historial = [
 ]
 
 const Votaciones = () => {
+
+    const [deleteModal, setDeleteModal] = React.useState(false)
+    const [shareModal, setShareModal] = React.useState(false)
+    const [search, setSearch] = React.useState('')
+
+    const filteredRooms = historial.filter(room => room.nombre.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+
     return (
         <div className='w-2/4 font-dmsans'>
             <h1 className='mb-10 text-4xl font-bold'>Votaciones</h1>
             <div className=' flex flex-col bg-secondaryGray pt-6 w-5/6 rounded-lg'>
                 <div className='pb-10 flex flex-col items-center'>
                     <form className='bg-primaryOrange rounded-full flex items-center h-8 justify-evenly gap-4 px-4'>
-                        <label className="busqueda text-secondaryWhite">Buscar:</label>
-                        <input type="text" name="busqueda" className='buesqueda bg-primaryOrange text-secondaryWhite mx-2 w-24' />
-                        <button type="submit">
-                            <Image src="/Images/lupa.png" alt="lupa" width={25} height={25} />
-                        </button>
+                        <input 
+                            type="text" 
+                            name="busqueda"
+                            placeholder="Buscar sala" 
+                            className='busqueda bg-primaryOrange text-secondaryWhite mx-2 w-24 placeholder-white px-2 text-xs' 
+                            value = {search}
+                            onChange={(event) => setSearch(event.target.value)}
+                        />
+                        <Image src="/Images/lupa.png" alt="lupa" width={25} height={25} />
                     </form>
                 </div>
                 <div className='flex gap-4 pl-10 pb-10 '>
@@ -34,19 +49,54 @@ const Votaciones = () => {
                 </div>
                 <div className='flex flex-col gap-4 px-10 pb-10'>
 
-                    {historial.map((sala, index) => 
+                    {filteredRooms.map((sala, index) => 
                     <div className='flex gap-2 justify-between' key={index}>
                         <div className={`w-3 h-3 ${sala.isActive ? 'bg-green-500' : 'bg-red-500'} rounded-full`}></div>
                         <p className='text-xs text-start w-full'>{sala.codigo} - {sala.nombre} | {sala.ganador} </p>
                         <div className="flex justify-end w-1/4 gap-4 items-center">
-                            <Image src="/Images/CoronaIcon.png" alt="corona" width={20} height={20} />
-                            <Image src="/Images/TrashIcon.png" alt="corona" width={20} height={20} />
-                            <Image src="/Images/ShareIcon.png" alt="corona" width={15} height={15} />
+                            <button>
+                                <Image 
+                                    src="/Images/CoronaIcon.png" 
+                                    alt="corona" 
+                                    width={20} 
+                                    height={20} 
+                                    className="cursor-pointer"
+                                />
+                            </button>
+                            <button onClick={() => setDeleteModal(!deleteModal)}>
+                                <Image
+                                    src="/Images/TrashIcon.png" 
+                                    alt="corona" 
+                                    width={20} 
+                                    height={20} 
+                                    className="cursor-pointer"
+                                />
+                            </button>
+                            <button onClick={() => setShareModal(!shareModal)}>
+                                <Image
+                                    src="/Images/ShareIcon.png" 
+                                    alt="corona" 
+                                    width={15} 
+                                    height={15} 
+                                    className="cursor-pointer"
+                                />
+                            </button>
                         </div>
                     </div>)}
-
                 </div>
             </div>
+            <ModalGeneral state = { deleteModal } changeState = { setDeleteModal }>
+                <ModalEliminarSala />
+            </ModalGeneral>
+            
+            <ModalGeneral state = { shareModal } changeState = { setShareModal }>
+                <ModalCopiar 
+                    image = {"/Images/ShareIcon.png"}
+                    title = "Comparte la sala con tus amigos"
+                    content = "Enlace: "
+                    code = {`https://votechoice.com/sala/${2985674}`}
+                />
+            </ModalGeneral>
         </div>
     )
 }
