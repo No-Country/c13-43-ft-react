4a7a96c4b284f535734dc8803e2556ba50d7e2cd
Martin Cosimano <martincosimanodev@gmail.com>
2023-09-06 16:28:57 -0300

    algunos cambios

--- src/app/login/[id]/page.jsx
+++ src/app/login/[id]/page.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import Atajos from '@/containers/Atajos'
 import Resultados from '@/components/Resultados'
@@ -12,12 +13,12 @@ export default async function Panel() {
     const session = await getServerSession(NextAuthOptions)
     // const [component, setComponent] = React.useState(true)
 
-    const component = true
+
     
     return (
         <div className="flex items-center mt-16 gap-10">
             <div className="w-1/4"></div>
-            {!!component ? <Resultados /> : <SinVotaciones />}
+            {true ? <Resultados /> : <SinVotaciones />}
             <Atajos />
         </div>
     )
