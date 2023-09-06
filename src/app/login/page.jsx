f57b098cbc4a0645f54f4480fae10d28fc0381dc
PabloMur <polillomurillo@gmail.com>
2023-09-06 16:33:53 -0300

    se agregaron cambios en login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,102 +1,115 @@
-"use client"
-import ImagePrincipal from '@/components/ImagePrincipal';
-import Password from '@/components/Password';
-import React from 'react';
-import { GoogleButton } from '@/components/GoogleButton';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-import { signIn } from 'next-auth/react';
-import ModalGeneral from '@/containers/ModalGeneral';
-import ModalRegister from '@/components/ModalRegister';
+"use client";
+import ImagePrincipal from "@/components/ImagePrincipal";
+import Password from "@/components/Password";
+import React, { useEffect } from "react";
+import { GoogleButton } from "@/components/GoogleButton";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import ModalGeneral from "@/containers/ModalGeneral";
+import ModalRegister from "@/components/ModalRegister";
+import { useSession } from "next-auth/react";
 
 const Login = () => {
   const router = useRouter();
   const [error, setError] = useState();
-  const [stateModal, setStateModal] = useState( false )
+  const [stateModal, setStateModal] = useState(false);
+  const { data: session, status } = useSession();
 
-  const handleSubmit = async (e) => {
-      e.preventDefault();
-      const data = new FormData(e.currentTarget);
+  useEffect(() => {
+    const userData = session?.user?.id;
+    router.push(`/login/${userData}`);
+  }, [status == "authenticated"]);
 
-      const signInResponse = await signIn("credentials", {
-          email: data.get("email"),
-          password: data.get("password"),
-          redirect: false,
-      });
-
-      if (signInResponse && !signInResponse.error) {
-          router.push("/login/1234");
-      } else {
-          console.log("Error: ", signInResponse);
-          setError("Inicio de sesión fallido: verifica tu email y contraseña")
-      }
+  const cerrarModal = async () => {
+    setStateModal(!stateModal);
   };
 
-return (
-  <>
-    <div className="flex gap-40 pt-8 pl-32">
-      <main className="flex-colum justify-center w-1/2 ">
-        <h1 className="text-secondaryBlack text-5xl font-bold font-dmsans flex justify-center">
-          {" "}
-          Iniciar sesión{" "}
-        </h1>
-        <div className="relative">
-          <form onSubmit={handleSubmit}>
-            <div className="my-4 pt-4">
-              <label
-                className="text-secondaryBlack font-dmsans font-medium"
-                htmlFor="email"
-              >
-                {" "}
-                EMAIL{" "}
-              </label>
-              <input
-                className="w-full border-b border-secondaryBlack bg-slate-50 outline-none "
-                type="email"
-                name="email"
-                id="email"
-              />
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = e.currentTarget;
+
+    const signInResponse = await signIn("credentials", {
+      email: data.get("email"),
+      password: data.get("password"),
+      redirect: false,
+    });
+
+    if (signInResponse && !signInResponse.error) {
+    } else {
+      console.log("Error: ", signInResponse);
+      setError("Inicio de sesión fallido: verifica tu email y contraseña");
+    }
+  };
+
+  return (
+    <>
+      <div className="flex gap-40 pt-8 pl-32">
+        <main className="flex-colum justify-center w-1/2 ">
+          <h1 className="text-secondaryBlack text-5xl font-bold font-dmsans flex justify-center">
+            {" "}
+            Iniciar sesión{" "}
+          </h1>
+          <div className="relative">
+            <form onSubmit={handleSubmit}>
+              <div className="my-4 pt-4">
+                <label
+                  className="text-secondaryBlack font-dmsans font-medium"
+                  htmlFor="email"
+                >
+                  {" "}
+                  EMAIL{" "}
+                </label>
+                <input
+                  className="w-full border-b border-secondaryBlack bg-slate-50 outline-none "
+                  type="email"
+                  name="email"
+                  id="email"
+                />
+              </div>
+              <Password nameLabel="CONTRASEÑA" name="password" />
+              <div className="mt-2">
+                {error && (
+                  <p className="font-medium font-dmsans text-red-600">
+                    {error}
+                  </p>
+                )}
+              </div>
+              <div className="flex justify-start items-center gap-8 mt-10">
+                <button className="bg-primaryPurple text-secondaryWhite w-5/12 font-dmsans font-medium py-2 rounded-full">
+                  {" "}
+                  INGRESAR{" "}
+                </button>
+              </div>
+            </form>
+            <div className="flex justify-center items-center gap-8  pt-4 absolute bottom-0 right-0 w-5/12">
+              <GoogleButton />
             </div>
-            <Password nameLabel="CONTRASEÑA" name="password" />
-            <div className='mt-2'>{error && <p className='font-medium font-dmsans text-red-600'>{error}</p>}</div>
-            <div className="flex justify-start items-center gap-8 mt-10">
-              <button className="bg-primaryPurple text-secondaryWhite w-5/12 font-dmsans font-medium py-2 rounded-full">
-                {" "}
-                INGRESAR{" "}
-              </button>
-            </div>
-          </form>
-          <div className="flex justify-center items-center gap-8  pt-4 absolute bottom-0 right-0 w-5/12">
-            <GoogleButton />
           </div>
-        </div>
 
-        <div className="flex gap-4 my-7">
-          <hr className="flex-grow border-secondaryBlack mt-3" />
-          <span className="text-secondaryBlack font-dmsans font-medium">
-            OR
-          </span>
-          <hr className="flex-grow border-secondaryBlack mt-3" />
-        </div>
+          <div className="flex gap-4 my-7">
+            <hr className="flex-grow border-secondaryBlack mt-3" />
+            <span className="text-secondaryBlack font-dmsans font-medium">
+              OR
+            </span>
+            <hr className="flex-grow border-secondaryBlack mt-3" />
+          </div>
 
-        <button 
-          className="text-primaryPurple font-dmsans font-medium border-primaryPurple border rounded-full w-full py-2"
-          onClick={() => setStateModal( !stateModal )}
-        >
-          {" "}
-          REGISTRATE{" "}
-        </button>
-      </main>
-      <ImagePrincipal />
-    </div>
-    <ModalGeneral
-      state = { stateModal }
-      changeState = { setStateModal }
-    >
-      <ModalRegister/>
-    </ModalGeneral>
+          <button
+            className="text-primaryPurple font-dmsans font-medium border-primaryPurple border rounded-full w-full py-2"
+            onClick={() => setStateModal(!stateModal)}
+          >
+            {" "}
+            REGISTRATE{" "}
+          </button>
+        </main>
+        <ImagePrincipal />
+      </div>
+      <ModalGeneral state={stateModal} changeState={setStateModal}>
+        <ModalRegister callback={cerrarModal} />
+      </ModalGeneral>
     </>
   );
 };
 
-export default Login
+export default Login;
--- src/components/ModalRegister.jsx
+++ src/components/ModalRegister.jsx
@@ -1,60 +1,91 @@
-'use client';
-import React from 'react';
-import Password from './Password';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { APICreateUser } from '@/lib/APICalls';
+"use client";
+import React from "react";
+import Password from "./Password";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { APICreateUser } from "@/lib/APICalls";
 
-const ModalRegister = () => {
-    const router = useRouter();
-    const [error, setError] = useState();
-    
-    const  handleSubmit = async ( event ) =>{
-        event.preventDefault();
-        const { nombre, email, passwordModal } = event.target;
-        const response = await APICreateUser( nombre.value, email.value, passwordModal.value );
-        console.log(response)
+const ModalRegister = (callback) => {
+  const router = useRouter();
+  const [error, setError] = useState();
 
-        if(response && !response.error) {
-            router.push("/login/1234");
-        }else {
-            console.log("Error: ", response);
-            setError("Creación de cuenta fallido: email existente")
-        }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const { nombre, email, passwordModal } = event.target;
+    const response = await APICreateUser(
+      nombre.value,
+      email.value,
+      passwordModal.value
+    );
+    if (response && !response.error) {
+      callback.callback();
+    } else {
+      console.log("Error: ", response);
+      setError("Creación de cuenta fallido: email existente");
     }
+  };
 
-    return (
-        <main>
-            <h1 className='text-secondaryBlack text-5xl font-bold font-dmsans flex justify-center'> Registrate </h1>
-            <form onSubmit={ handleSubmit }>
-                <div className='my-6'>
-                    <label className='text-secondaryBlack font-dmsans font-medium' htmlFor='nombre'> NOMBRE Y APELLIDO </label>
-                    <input className="w-full border-b border-secondaryBlack bg-slate-50 outline-none " type="text" name="nombre" id='nombre'/>
-                </div>
+  return (
+    <main>
+      <h1 className="text-secondaryBlack text-5xl font-bold font-dmsans flex justify-center">
+        {" "}
+        Registrate{" "}
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <div className="my-6">
+          <label
+            className="text-secondaryBlack font-dmsans font-medium"
+            htmlFor="nombre"
+          >
+            {" "}
+            NOMBRE Y APELLIDO{" "}
+          </label>
+          <input
+            className="w-full border-b border-secondaryBlack bg-slate-50 outline-none "
+            type="text"
+            name="nombre"
+            id="nombre"
+          />
+        </div>
 
-                <div className='my-6'>
-                    <label className='text-secondaryBlack font-dmsans font-medium' htmlFor='emailModal'> EMAIL </label>
-                    <input className="w-full border-b border-secondaryBlack bg-slate-50 outline-none " type="email" name="email" id='emailModal'/>
-                </div>
+        <div className="my-6">
+          <label
+            className="text-secondaryBlack font-dmsans font-medium"
+            htmlFor="emailModal"
+          >
+            {" "}
+            EMAIL{" "}
+          </label>
+          <input
+            className="w-full border-b border-secondaryBlack bg-slate-50 outline-none "
+            type="email"
+            name="email"
+            id="emailModal"
+          />
+        </div>
 
-                <Password
-                    nameLabel = 'CONTRASEÑA'
-                    name = 'passwordModal'
-                    id= 'passwordModal'
-                />
+        <Password
+          nameLabel="CONTRASEÑA"
+          name="passwordModal"
+          id="passwordModal"
+        />
 
-                <Password
-                    nameLabel = 'REPETIR CONTRASEÑA'
-                    name = 'passwordModal2'
-                />
-                <div className='my-2'>{error && <p className='font-medium font-dmsans text-red-600'>{error}</p>}</div>
-                
-                <button className='bg-primaryPurple text-secondaryWhite font-dmsans font-medium w-full py-2 rounded-full'> REGISTRAR </button>
+        <Password nameLabel="REPETIR CONTRASEÑA" name="passwordModal2" />
+        <div className="my-2">
+          {error && (
+            <p className="font-medium font-dmsans text-red-600">{error}</p>
+          )}
+        </div>
 
-                {/* <button className='text-primaryPurple font-dmsans font-medium border-primaryPurple border rounded-full w-full py-2'> INICIAR SESIÓN </button> */}
-            </form>
-        </main>
-    )
-}
+        <button className="bg-primaryPurple text-secondaryWhite font-dmsans font-medium w-full py-2 rounded-full">
+          {" "}
+          REGISTRAR{" "}
+        </button>
+
+        {/* <button className='text-primaryPurple font-dmsans font-medium border-primaryPurple border rounded-full w-full py-2'> INICIAR SESIÓN </button> */}
+      </form>
+    </main>
+  );
+};
 
 export default ModalRegister;
