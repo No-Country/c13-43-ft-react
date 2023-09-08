95ffa212d8b526a07a552b1f58836a82fdb2d785
PabloMur <polillomurillo@gmail.com>
2023-09-08 17:47:59 -0300

    cambios en endpoit vote

--- src/app/api/vote/route.js
+++ src/app/api/vote/route.js
@@ -2,24 +2,21 @@ import { NextResponse } from "next/server";
 import { firestoreDB } from "@/lib/firebaseConn";
 
 export async function PUT(request) {
-  const { searchParams } = new URL(request.url);
-  const optionID = searchParams.get("optionId");
-  const roomId = searchParams.get("roomId");
-  const votantEmail = searchParams.get("voterEmail");
-
+  const body = await request.json();
+  const { optionId, roomId, email } = body;
   const roomRef = firestoreDB.collection("rooms").doc(roomId);
   const currentRoomData = (await roomRef.get()).data();
-  const currentOption = currentRoomData.options[optionID];
+  const currentOption = currentRoomData.options[optionId];
   const currentVotedBy = currentOption.votedBy;
-  const newVotedByEntry = { voterEmail: votantEmail };
+  const newVotedByEntry = { votant: email };
 
   const updatedTimesVoted = currentOption.timesVoted + 1;
-  const updatedVotedBy = [...currentVotedBy, newVotedByEntry];
+  const updatedVotedBy = { ...currentVotedBy, newVotedByEntry };
 
   await roomRef.update({
-    [`options.${optionID}.timesVoted`]: updatedTimesVoted,
-    [`options.${optionID}.votedBy`]: updatedVotedBy,
+    [`options.${optionId}.timesVoted`]: updatedTimesVoted,
+    [`options.${optionId}.votedBy`]: updatedVotedBy,
   });
 
-  return NextResponse.json(updatedVotedBy);
+  return NextResponse.json({ currentOption });
 }
