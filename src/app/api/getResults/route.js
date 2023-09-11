d567f4703800ea972c6f1b161685d14d670304ed
PabloMur <polillomurillo@gmail.com>
2023-09-11 14:17:44 -0300

    he agregado el endpoint getResults, el cual brinda al front la data necesaria

--- src/app/api/getResults/route.js
+++ src/app/api/getResults/route.js
@@ -1,13 +1,45 @@
-import { NextResponse } from "next/server";
 import { firestoreDB } from "@/lib/firebaseConn";
-export async function GET(request) {
-  //recibir la id de la room
-  //se debe obtener los resultados de las opciones y los votos
-  const { searchParams } = new URL(request.url);
-  const roomId = searchParams.get("roomId");
-  const resultsRef = await firestoreDB.collection("rooms").doc(roomId).get();
-  const resultsData = resultsRef.data().options;
-  return NextResponse.json({ resultsData });
-}
+import { NextResponse } from "next/server";
 
-//https://codevoweb.com/build-a-simple-api-in-next-js-13-app-directory/ puede servir como referencia
+export async function GET(request) {
+    try {
+        // Recibir la ID de la sala
+        const { searchParams } = new URL(request.url);
+        const roomId = searchParams.get("roomId");
+
+        // Obtener el documento de la sala
+        const roomDoc = await firestoreDB.collection("rooms").doc(roomId).get();
+
+        // Verificar si el campo "expired" está en true
+        if (roomDoc.exists) {
+            const roomData = roomDoc.data();
+            if (roomData.expired === true) {
+                // Extraer los resultados de las opciones y los votos
+                const resultsData = Object.values(roomData.options);
+
+                // Obtener la cantidad total de participantes
+                const totalParticipants = roomData.participants.length;
+
+                // Calcular el porcentaje de votos en cada opción
+                const resultsWithPercentage = resultsData.map((option) => ({
+                    ...option,
+                    percentage: (option.timesVoted / totalParticipants) * 100,
+                }));
+
+                return NextResponse.json({
+                    resultsWithPercentage,
+                    totalParticipants,
+                });
+            } else {
+                // Si el campo "expired" no está en true, devolver un mensaje
+                return NextResponse.json({ message: "La sala no ha expirado" });
+            }
+        } else {
+            // Si no se encuentra la sala con el ID dado
+            return NextResponse.json({ message: "Sala no encontrada" });
+        }
+    } catch (error) {
+        console.error("Error al obtener datos de Firestore:", error);
+        return NextResponse.error("Error al obtener datos de Firestore");
+    }
+}
