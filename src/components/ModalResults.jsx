9f740070207ecdbab8e35d25e0dbb66caf8767ac
mbernallaura <mbernallaura@gmail.com>
2023-09-12 16:26:01 -0500

    Ya casi listo lo de resultados en panel

--- src/components/ModalResults.jsx
+++ src/components/ModalResults.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Options from './Options';
 import { APIGetRoomOptions } from '@/lib/APICalls';
+import Loader from "./Loader";
 
 const arrOptions = [
     {opcion: 'Viernes 20 de Septiembre - 22h', votos: '5 de 10 votaron esta opcion'},
@@ -11,14 +12,17 @@ const arrOptions = [
     {opcion: 'Viernes 20 de Septiembre - 12h', votos: '3 de 10 votaron esta opcion'}
 ]
 
-const ModalResults = ({ roomId, problem }) => {
+const ModalResults = ({ roomId, problem, participants }) => {
     const [finishResults, setFinishResults] = useState([]);
+    const [ loaderActive, setLoaderActive ] = useState( false );
 
     useEffect(() => {
         const results = async() =>{
             try {
+                setLoaderActive( true );
                 const arrayResults = await APIGetRoomOptions( roomId );
                 setFinishResults( arrayResults );
+                setLoaderActive( false )
                 console.log(arrayResults);
             } catch (error) {
                 
@@ -30,17 +34,20 @@ const ModalResults = ({ roomId, problem }) => {
     
 
     return (
-        <div className='flex justify-center items-center font-dmsans'>
-            <div className='flex flex-col items-center p-2 pb-10'>
-                <div className='items-center flex flex-col p-6 pb-0 pt-0'>
-                    <h2 className='text-primaryPurple font-dmsans font-bold text-3xl mb-3'>Resultados</h2>
-                    <p className='mb-4'>Acá los resultados de tu votación más reciente: <br/> <span className='font-semibold'>{ problem }</span></p>
-                    <div className='items-center flex flex-col w-full mt-1'>
-                        {finishResults.map((option, index) => <Options key={option.id} opcion={option.title} percentage = { option.percentage } timesVoted ={ option.timesVoted } />)}
+        <>
+            <Loader active ={ loaderActive }/>
+            <div className='flex justify-center items-center font-dmsans'>
+                <div className='flex flex-col items-center p-2 pb-10'>
+                    <div className='items-center flex flex-col p-6 pb-0 pt-0'>
+                        <h2 className='text-primaryPurple font-dmsans font-bold text-3xl mb-3'>Resultados</h2>
+                        <p className='mb-4'>Acá los resultados de tu votación más reciente: <br/> <span className='font-semibold'>{ problem }</span></p>
+                        <div className='items-center flex flex-col w-full mt-1'>
+                            {finishResults.map((option, index) => <Options key={option.id} opcion={option.title} percentage = { option.percentage } timesVoted ={ option.timesVoted } participants={ participants } />)}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
 
     )
 }
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Options = ({opcion, percentage, timesVoted}) => {
+const Options = ({opcion, percentage, timesVoted, participants}) => {
     return (
         <div className='w-full mb-5'>
             <div className='flex mb-3'>
@@ -9,7 +9,7 @@ const Options = ({opcion, percentage, timesVoted}) => {
             <div className="w-full h-2 border border-primaryPurple rounded mb-2">
                 <div className="h-full bg-primaryPurple rounded" style={{ width:`${percentage}%` }}></div>
             </div>
-            <p className='text-sm'>{ timesVoted } Participante/es </p>
+            <p className='text-sm'>{ timesVoted } de { participants } votaron esta opción </p>
         </div>
     )
 }
