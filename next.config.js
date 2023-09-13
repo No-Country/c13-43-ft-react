793a6169c16f47adeb1386dd368ba05656ceea90
Alejou343 <alejandro.auribe1@gmail.com>
2023-09-13 00:45:06 -0500

    Intento deploy #5

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    prerender: {
+      initialStatus: 200, // Un código de estado HTTP válido
+      // Otras opciones de configuración de prerenderizado
+      // ...
+    },
+  },
 }
 
 module.exports = nextConfig
