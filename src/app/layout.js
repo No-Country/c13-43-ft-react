e6c82a7626a85e0cf5a8b0ce4831fbe6e5980e87
Alejou343 <alejandro.auribe1@gmail.com>
2023-09-13 14:24:40 -0500

    Intento deploy #14

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,11 @@
+"use client"
 import Navbar from "@/components/Navbar"
 import AuthProvider from "@/context/AuthProvider"
 import '@/styles/globals.css'
 import { getServerSession } from "next-auth"
 import { NextAuthOptions } from "./api/auth/[...nextauth]/options"
 
+export const dynamic = 'force-dynamic'
 
 export default async function RootLayout({ children }) {
   const session = await getServerSession(NextAuthOptions)
