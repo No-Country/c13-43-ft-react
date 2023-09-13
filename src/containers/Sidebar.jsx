0785f08a3309103b04e5daa70afcbbfb7af91c94
Alejou343 <alejandro.auribe1@gmail.com>
2023-09-12 19:31:42 -0500

    <Fix> El contenido de sidebar ahora depende de la url

--- src/containers/Sidebar.jsx
+++ src/containers/Sidebar.jsx
@@ -5,12 +5,29 @@ import { ChangeLi } from '../components/ChangeLi';
 import { useSession } from 'next-auth/react';
 import ModalGeneral from './ModalGeneral';
 import ModalConfirmLogout from '@/components/ModalConfirmLogout';
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
     const { data: session } = useSession();
     const userData = session?.user?.id
     const [activeIndex, setActiveIndex] = useState(0);
     const [confirmLogout, setConfirmLogout] = useState(false);
+    const path = usePathname()
+
+    React.useEffect(() => {
+        const regex = /\/(votaciones|perfil)?$/
+        const values = ['', 'votaciones', 'perfil']
+    
+        if (path.match(regex) == null) {
+            setActiveIndex(0)
+        } else {
+            setActiveIndex(values.indexOf(path.match(regex)[1]))
+        }
+    }, [path])
+    
+    // console.log(path)
+    // console.log(values.indexOf(path.match(regex)[1]))
+
 
     const imgsM = {
         panel: '/Images/panel/panelM.svg',
